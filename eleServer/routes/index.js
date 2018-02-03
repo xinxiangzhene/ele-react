58f1f813711a5f71ce7f82e6acac3d8b0b9cb6e9
guojiongwei <1030286235@qq.com>
2018-02-03 11:42:17 +0800

    后台修复

--- eleServer/routes/index.js
+++ eleServer/routes/index.js
@@ -38,15 +38,13 @@ var str = "";
 
 });
 
-
+//搜索城市具体地区
 router.get('/list', function(req, res, next) {
 	var obj = url.parse(req.url,true).query;
-			console.log(obj.city)
-			var city = obj.city
 	 var options = {
     hostname: "www.ele.me",
     port: 443,
-    path: '/restapi/v2/pois?extras%5B%5D=count&geohash=wt9nuhpydtmt&keyword='+city+'&limit=20&type=nearby',
+    path: '/restapi/v2/pois?extras%5B%5D=count&geohash='+obj.geohash+'&keyword='+obj.city+'&limit=20&type=nearby',
     methods:"GET"
 	};
 var str = "";
@@ -68,86 +66,78 @@ var str = "";
 
 });
 
-//城市附近
-router.get('/cityList', function(req, res, next) {
-	 var options = {
-    hostname: "www.ele.me",
-    port: 443,
-    path: "/restapi/shopping/restaurants?extras%5B%5D=activities&geohash=ww0vd0wxsj6&latitude=34.72086&limit=24&longitude=113.65168&offset=0&terminal=web",
-    methods:"GET"
-	};
-var str = "";
- const reqData = https.request(options, (myData) =>{
-    myData.on("data", (data) => {
-      str += data;
-   
-      setTimeout(function(){
-      	   res.send(str)
-      },500)
-    })
-    myData.on("error", () =>{
-      console.log("15")
-    })
-    myData.on("end", () => {
-      console.log(1)
-    })
-  })
- reqData.on("error", () => {
-   console.log("22")
- })
- reqData.end(str);
-});
-
-
-//城市附近
-router.get('/product', function(req, res, next) {
-	 var options = {
-    hostname: "www.ele.me",
-    port: 443,
-    path: "/restapi/shopping/v2/menu?restaurant_id=468660",
-    methods:"GET"
-	};
-var str = "";
- const reqData = https.request(options, (myData) =>{
-    myData.on("data", (data) => {
-      str += data;
-   
-      setTimeout(function(){
-      	   res.send(str)
-      },200)
-    })
-    myData.on("error", () =>{
-      console.log("15")
-    })
-    myData.on("end", () => {
-      console.log(1)
-    })
-  })
- reqData.on("error", () => {
-   console.log("22")
- })
- reqData.end(str);
-});
-
-
-
-
-
-
-//http://www.yiguo.com/Handler/InitLayOut?r=0.5299923408118798&_=1517455967128
-
-router.get('/new', function(req, res, next) {
+//附近商店列表
+router.get('/shopList', function(req, res, next) {
 	var obj = url.parse(req.url,true).query;
-			console.log(obj.city)
-			var city = obj.city
 	 var options = {
-    hostname: "www.yiguo.com",
-    port: 80,
-    path: '/Handler/InitLayOut?r=0.5299923408118798&_=1517455967128',
+    hostname: "www.ele.me",
+    port: 443,
+    path: "/restapi/shopping/restaurants?extras%5B%5D=activities&geohash="+obj.geohash+"&latitude="+obj.latitude+"&limit=24&longitude="+obj.longitude+"&offset=0&terminal=web",
     methods:"GET"
 	};
 var str = "";
- const reqData = http.request(options, (myData) =>{
+ const reqData = https.request(options, (myData) =>{
+    myData.on("data", (data) => {
+      str += data;
+    })
+    myData.on("error", () =>{
+      console.log("15")
+    })
+    myData.on("end", () => {
+    	 res.send(str)
+      console.log(1)
+    })
+  })
+ reqData.on("error", () => {
+   console.log("22")
+ })
+ reqData.end(str);
+});
+
+
+//店铺详情
+router.get('/shopDetail', function(req, res, next) {
+	obj = url.parse(req.url,true).query;
+	 var options = {
+    hostname: "www.ele.me",
+    port: 443,
+    path: "/restapi/shopping/v2/menu?restaurant_id="+obj.id+"",
+    methods:"GET"
+	};
+var str = "";
+ const reqData = https.request(options, (myData) =>{
+    myData.on("data", (data) => {
+      str += data;
+    })
+    myData.on("error", () =>{
+      console.log("15")
+    })
+    myData.on("end", () => {
+    	 res.send(str)
+      console.log(1)
+    })
+  })
+ reqData.on("error", () => {
+   console.log("22")
+ })
+ reqData.end();
+});
+
+
+
+
+//根据经纬度获取geohash信息
+router.get('/geohash', function(req, res, next) {
+	var obj = url.parse(req.url,true).query;
+		console.log(obj)
+	 var options = {
+    hostname: "www.ele.me",
+    port: 443,
+    path: '/restapi/bgs/poi/reverse_geo_coding?latitude='+obj.latitude+'&longitude='+obj.longitude+'',
+    methods:"GET"
+	};
+var str = "";
+ const reqData = https.request(options, (myData) =>{
     myData.on("data", (data) => {
       str += data;
     })
@@ -162,6 +152,5 @@ var str = "";
    console.log("22")
  })
  reqData.end();
-
 });
 module.exports = router;
